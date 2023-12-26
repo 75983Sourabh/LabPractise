fb9f2b41c0626d2c7ceadf1b1bdbe80260bf48b4
Sourabh Lowanshi <126318639+75983Sourabh@users.noreply.github.com>
2023-12-26 18:42:39 +0530

    Update Program.cs

--- Practise Dotnet/DBDelete/Program.cs
+++ Practise Dotnet/DBDelete/Program.cs
@@ -1,11 +1,11 @@
-﻿using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;  //using package of MySql.Data ------>Command----->dotnet add package MySql.Data
 System.Console.WriteLine("Welcome to The Deletion Command of Sourabh");
 
-MySqlConnection conn=new MySqlConnection();
+MySqlConnection conn=new MySqlConnection(); //--------->creating a connection
 conn.ConnectionString="server=192.168.10.150;port=3306;user=dac40;password=welcome;database=dac40";
 
 System.Console.WriteLine("Enter the id of product you want to delete");
-int id=int.Parse(Console.ReadLine());
+int id=int.Parse(Console.ReadLine());//--->parsing a 
 string query="delete  from product where id=@id";
 
 MySqlCommand command=new MySqlCommand(query,conn);
